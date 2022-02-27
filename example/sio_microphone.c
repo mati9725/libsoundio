5658fa1a2c1bc77cfef64a39af4f6ea93a76bd6e
mati9725 <mati9725@wp.pl>
2022-02-27 10:27:45 +0100

    trying to print

--- example/sio_microphone.c
+++ example/sio_microphone.c
@@ -94,7 +94,7 @@ static void read_callback(struct SoundIoInStream* instream, int frame_count_min,
             for (int frame = 0; frame < frame_count; frame += 1) {
                 for (int ch = 0; ch < instream->layout.channel_count; ch += 1) {
                     printf("step %d", areas[ch].step);
-                    printf("char %d", (int)areas[ch].ptr);
+                    printf("char %d, %d, %d, %d", (int)areas[ch].ptr[0], (int)areas[ch].ptr[1], (int)areas[ch].ptr[2], (int)areas[ch].ptr[3]);
                     memcpy(write_ptr, areas[ch].ptr, instream->bytes_per_sample);
                     areas[ch].ptr += areas[ch].step;
                     write_ptr += instream->bytes_per_sample;
